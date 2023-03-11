18fd63a84836828f35519e07f1695dee632e20cd
Bear Walker <72546878+chrisdenmark84@users.noreply.github.com>
2023-03-11 14:56:11 +0000

    Add folder structure and file templates

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Dice Simulator allows you to replicate the rolling of die for use with table top gaming.
 
-You can simulate the rolling of a number of die and have the rolls returned to you. 
+You can simulate the rolling of a number of die and have the rolls returned to you.
---
+++ public/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/dscss.css" />
+    <title>Dice Simulator</title>
+    <?php
+    include("../src/generate.php");
+    ?>
+</head>
+
+<body>
+
+    <!-- Header of document -->
+    <header class="main-header">
+        <h1><span class="logo"><img src="img/logo-aos-new.png" alt="Dice Simulator" /></span>Dice Simulator</h1>
+    </header>
+
+    <article class="main-description">
+        <p>The <strong>Dice Simulator</strong> is a web application designed to allow you to create battles for use in the Warhammer Age of Sigmar game by Games Workshop Limited.</p>
+    </article>
+
+    <!-- Input form for roll -->
+    <div class="form">
+        <form method="post">
+            <label for="quantity">Number of die: </label>
+            <input type="number" id="quantity" name="quantity" min="1" max="1000" value="<?php echo $numLastGen; ?>">
+            <input type="submit" name="generateBattle" class="generateBattle" value="Generate Battle" />
+        </form>
+    </div>
+
+    <!-- Roll Output -->
+    <div class="allresults">
+        <?php
+
+
+        ?>
+    </div>
+</body>
+
+</html>
