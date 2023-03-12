a0407f188335c5df50262f5b9c5b3957d69b5aac
Bear Walker <72546878+chrisdenmark84@users.noreply.github.com>
2023-03-12 22:49:53 +0000

    Add basic working functionality of dice roll

--- public/index.php
+++ public/index.php
@@ -11,10 +11,21 @@
 
 <body>
 
+    <?php
+    if (isset($_POST['quantity'])) {
+        $numLastGen = $_POST['quantity'];
+    } else {
+        $numLastGen = 1;
+    }
+    ?>
+
+
     <!-- Header of document -->
     <header class="main-header">
         <h1><span class="logo"><img src="img/logo-aos-new.png" alt="Dice Simulator" /></span>Dice Simulator</h1>
     </header>
+
+    <div class="hidden" style="font-family: 'Pompei Std'; display: none">preload</div>
 
     <article class="main-description">
         <p>The <strong>Dice Simulator</strong> is a web application designed to allow you to roll dice for tabletop games.</p>
@@ -25,7 +36,7 @@
         <form method="post">
             <label for="quantity">Number of die: </label>
             <input type="number" id="quantity" name="quantity" min="1" max="1000" value="<?php echo $numLastGen; ?>">
-            <input type="submit" name="generateBattle" class="generateBattle" value="Generate Battle" />
+            <input type="submit" name="generateRolls" class="generateRolls" value="Generate Rolls" />
         </form>
     </div>
 
@@ -33,9 +44,28 @@
     <div class="allresults">
         <?php
 
+        if (isset($_POST['generateRolls'])) {
+            $num_rolls = $_POST['quantity'];
 
+            // I'm creating a new instance of the Generate Class calld $generateBattles
+            $generateRolls = new generate;
+
+            //Im calling the generateBattle function and then looping through it here
+            foreach ($generateRolls->generateRolls($num_rolls) as $roll) {
+
+                echo sprintf(
+                    '<div class="result">
+                    <h3>Die %d</h3>
+                    <h4>Result: %d</h4>
+                </div>',
+                    $roll['die'],
+                    $roll['result']
+                );
+            }
+        }
         ?>
     </div>
+
 </body>
 
 </html>
--- src/generate.php
+++ src/generate.php
@@ -1,1 +1,27 @@
 <?php
+
+class generate
+{
+
+    private $result = 0;
+
+    function generateRolls($num_rolls)
+    {
+        if ($num_rolls > 1000) {
+            exit("<div class=\"result\"><h3>Error</h3><p>Roll less dice, you mad man</p>");
+        }
+        $count = 1;
+
+        $rolls = [];
+
+        while ($count <= $num_rolls) {
+
+            $result = rand(1, 6);
+
+            $rolls[$count]['die'] = $count;
+            $rolls[$count]['result'] = $result;
+            $count++;
+        }
+        return $rolls;
+    }
+};
